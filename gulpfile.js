0d94be082397738cd76ead9a2e1041f4d64c5945
vano_zap <vano.gorbik@rambler.ru>
2016-09-13 01:25:00 +0300

    dropdown

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
  
 gulp.task('sass', function () {
   return gulp.src('build/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(concat('style.css'))
     .pipe(gulp.dest('layout/css'));
 });
  
