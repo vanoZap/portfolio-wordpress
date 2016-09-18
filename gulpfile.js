9083396d4b63f8a6ac12364361c83340597817e7
vano_zap <vano.gorbik@rambler.ru>
2016-09-18 18:55:10 +0300

    styling menu

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
  
 gulp.task('sass', function () {
   return gulp.src('build/sass/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(concat('style.css'))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('layout/css'));
 });
  
